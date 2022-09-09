5e3fd56d2d12809071d60bf035f3043b3e31a9ff
your name <you@example.com>
2022-09-09 02:40:47 -0700

    all

---
+++ advancedOps1.c
@@ -0,0 +1,103 @@
+#include "monty.h"
+
+/**
+ * pchar - prints the int at the top of the stack as char
+ * @stack: stack given by main
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void pchar(stack_t **stack, unsigned int line_cnt)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_cnt);
+		exit(EXIT_FAILURE);
+		return;
+	}
+	if (isascii((*stack)->n) == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_cnt);
+		exit(EXIT_FAILURE);
+		return;
+	}
+	printf("%c\n", (*stack)->n);
+}
+
+/**
+ * pstr - prints the contents of a stack_t stack as a string
+ * @stack: stack given by main
+ * @line_cnt: line counter for error messages
+ *
+ * Return: nothing
+ */
+void pstr(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		if (current->n <= 0 || current->n > 127)
+			break;
+		putchar((char) current->n);
+		current = current->next;
+	}
+	putchar('\n');
+}
+
+/**
+* rotl - rotates the first element of the stack
+* @stack: stack head
+* @line_count: line count
+*
+* Return: void
+*/
+void rotl(stack_t **stack, unsigned int line_count)
+{
+	stack_t *left;
+	stack_t *right;
+
+	(void) line_count;
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+
+	left = right = *stack;
+
+	while (right->next) /* move the right pointer to the last node */
+		right = right->next;
+	right->next = left; /* a circle infinite linked list loop */
+	left->prev = right;
+	*stack = left->next; /* so we cut the link between the 0 and 1 element */
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
+
+/**
+* rotr - rotates the last node of a stack_t stack
+* @stack: stack head
+* @line_count: line count
+*
+* Return: void
+*/
+void rotr(stack_t **stack, unsigned int line_count)
+{
+	stack_t *bottom;
+	stack_t *prev;
+
+	(void) line_count;
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+
+	bottom = *stack;
+
+	while (bottom->next)
+		bottom = bottom->next;
+
+	prev = bottom->prev;
+	bottom->next = *stack;
+	bottom->prev = NULL;
+	prev->next = NULL;
+	(*stack)->prev = bottom;
+	*stack = bottom;
+}
+
---
+++ mandatoryOps.c
@@ -0,0 +1,114 @@
+#include "monty.h"
+
+/**
+ * push - push element into the stack
+ * @stack: stack given by main
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_cnt)
+{
+	char *n = global.argument;
+
+	if ((is_digit(n)) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+
+	if (global.data_struct == 1)
+	{
+		if (!add_node(stack, atoi(global.argument)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (!queue_node(stack, atoi(global.argument)))
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	print_stack(*stack);
+}
+
+/**
+* pop - pops the very top element of the stack
+* @stack: stack given by main in start.c
+* @line_cnt: line number for error messages
+*
+* Return: void
+*/
+void pop(stack_t **stack, unsigned int line_cnt)
+{
+	stack_t *tmp = NULL;
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*stack)->next;
+	free(*stack);
+	*stack = tmp;
+	if (!*stack)
+		return; /* prevents errors cause next line might assign a NULL */
+	(*stack)->prev = NULL;
+}
+
+/**
+ * pint - print the value at the top of the stack
+ * @stack: stack given by main in start.c
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_cnt)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
+		exit(EXIT_FAILURE);
+
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * swap -  swaps data from top to previous
+ * @stack: stack given by main
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void swap(stack_t **stack, unsigned int line_cnt)
+{
+	stack_t *tmp = NULL;
+	int tmp_n = 0;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_cnt);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	tmp_n = tmp->n;
+	tmp->n = tmp_n;
+
+	tmp->n = tmp->next->n;
+	tmp->next->n = tmp_n;
+}
